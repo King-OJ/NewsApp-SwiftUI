e3824ea2acfca96d1d9042106d862021487d8b5d
Clement Ojiguo <clementojigs@hotmail.com>
2024-07-11 10:41:43 -0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by King OJ on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
